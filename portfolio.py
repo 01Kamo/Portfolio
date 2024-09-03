348d612f903766ed9962495420f41ab539ff7d9f
Kamogelo Nkwana <122813186+01Kamo@users.noreply.github.com>
2024-09-03 16:06:32 +0200

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -343,8 +343,7 @@ def main():
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhNZ-oA857LBER7nEhPzLXCmLyIfHwbhRV4w&s" alt="Sand Technologies Logo" />
    <div>
    <strong>Sand Technologies</strong><br />
-   Data Science Intern(September 2024 - Present)<br />
-   - Collaborating on the development and deployment of machine learning models to solve real-world business problems.
+   Data Science Intern(September 2024 - Present)<br /> - Collaborating on the development and deployment of machine learning models to solve real-world business problems.
    
    - Conducting data cleaning, preprocessing, and exploratory data analysis to uncover key insights and trends.
    
